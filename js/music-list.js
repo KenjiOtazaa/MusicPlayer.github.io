99dd69a601fc97b8d02f7efd63ae0632d39a7b7f
Kenji <120694300+KenjiOtazaa@users.noreply.github.com>
2022-12-17 19:47:59 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "KenjiOtaza",
+    artist: "KenjiOtaza",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Sun and Moon",
+    artist: "Anees",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "At My Worst",
+    artist: "Pink Sweat$",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Carpenters Greatest Hits Album ",
+    artist: "Carpenters",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "20 OPM!International Songs",
+    artist: "Fingerstyle Guitar Covers Compilation",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "EMPILIGHT",
+    artist: "JONAS",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
